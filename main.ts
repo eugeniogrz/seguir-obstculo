857b89bb8a4b3d0ec0039af336088459e4feb2fb
eugeniogrz <46166781+eugeniogrz@users.noreply.github.com>
2022-04-24 23:01:33 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ basic.forever(function () {
     Distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
     if (Distancia > 0 && Distancia <= 5) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 30)
-    } else if (Distancia > 10 && Distancia <= 10) {
+    } else if (Distancia > 5 && Distancia <= 10) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
